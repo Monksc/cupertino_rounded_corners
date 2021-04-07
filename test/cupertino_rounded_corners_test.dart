ae52a0488ced1b7b41114d628a96454fa54b47bd
monksc <ccmonks32@gmail.com>
2021-04-07 17:29:11 -0400

    Added in an example and changed test code so it worked ;).

--- test/cupertino_rounded_corners_test.dart
+++ test/cupertino_rounded_corners_test.dart
@@ -1,16 +1,16 @@
-import 'package:test/test.dart';
+// import 'package:test/test.dart';
 import 'package:cupertino_rounded_corners/cupertino_rounded_corners.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final CupertinoCard card = new CupertinoCard();
-    expect(0, 0);
-    /*
-    final calculator = new Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-    */
-  });
+  // test('adds one to input values', () {
+  //   final CupertinoCard card = new CupertinoCard();
+  //   expect(0, 0);
+  //   /*
+  //   final calculator = new Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  //   expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  //   */
+  // });
 }
